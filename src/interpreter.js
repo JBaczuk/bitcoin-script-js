ebbdf782461aa215c7f03da34cba2ac7f011c1d6
Jordan Baczuk <Jordan.Baczuk@gmail.com>
2018-12-27 14:58:58 -0700

    Add support for detecting invalid or disabled opcodes.

--- src/interpreter.js
+++ src/interpreter.js
@@ -19,6 +19,7 @@ class Interpreter {
       throw Error('No script provided')
     }
     // Validate opcode
+<<<<<<< HEAD
     if (!Script.opcodeIsValid(this.script.at(this.programCounter))) {
       throw Error(`Invalid opcode ${this.script.at(this.programCounter).toString(16)}`)
     } else {
@@ -39,12 +40,27 @@ class Interpreter {
       } else {
         let opcode = Script.wordForOpcode(this.script.at(this.programCounter))
         if (Script.wordIsDisabled(opcode)) {
+=======
+    if (!bscript.opcodes.opcodeIsValid(this.script[this.pc])) {
+      throw Error(`Invalid opcode ${this.script[this.pc].toString(16)}`)
+    } else {
+      if (this.script[this.pc] <= 0x4b) {
+        this.stack.push(this.script[this.pc])
+        this.pc += 1
+      } else {
+        let opcode = bscript.opcodes.wordForOpcode(this.script[this.pc])
+        if (bscript.opcodes.wordIsDisabled(opcode)) {
+>>>>>>> Add support for detecting invalid or disabled opcodes.
           throw Error(`Disabled opcode ${opcode}`)
         }
         // TODO: execute the op_code
         switch (opcode) {
           default:
+<<<<<<< HEAD
             throw Error(`Invalid opcode ${this.script.at(this.programCounter).toString(16)}`)
+=======
+            throw Error(`Invalid opcode ${this.script[this.pc].toString(16)}`)
+>>>>>>> Add support for detecting invalid or disabled opcodes.
         }
       }
     }
