86b0971cd721499a54c793e73f95997117e2d72a
Jordan Baczuk <Jordan.Baczuk@gmail.com>
2019-02-13 21:47:08 -0700

    refactor into script class

--- src/interpreter.js
+++ src/interpreter.js
@@ -2,30 +2,15 @@ const Script = require('./script')
 
 class Interpreter {
   constructor (script) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     if (!(script instanceof Script)) {
       throw Error('script must be instance of Script')
     }
     this.script = script
     this.programCounter = 0
     this.programEnd = this.script.length
-=======
-    if (!Buffer.isBuffer(script)) {
-      throw Error('Script must be a buffer')
-    }
     this.script = script
     this.programCounter = 0
     this.programEnd = script.length
->>>>>>> fix pushdata
-=======
-    if (!(script instanceof Script)) {
-      throw Error('script must be instance of Script')
-    }
-    this.script = script
-    this.programCounter = 0
-    this.programEnd = this.script.length
->>>>>>> squashme
     this.stack = []
   }
 
@@ -37,9 +22,6 @@ class Interpreter {
       throw Error('No script provided')
     }
     // Validate opcode
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     if (!Script.opcodeIsValid(this.script.at(this.programCounter))) {
       throw Error(`Invalid opcode ${this.script.at(this.programCounter).toString(16)}`)
     } else {
@@ -60,62 +42,12 @@ class Interpreter {
       } else {
         let opcode = Script.wordForOpcode(this.script.at(this.programCounter))
         if (Script.wordIsDisabled(opcode)) {
-=======
-    if (!bscript.opcodes.opcodeIsValid(this.script[this.pc])) {
-      throw Error(`Invalid opcode ${this.script[this.pc].toString(16)}`)
-=======
-    if (!bscript.opcodes.opcodeIsValid(this.script[this.programCounter])) {
-      throw Error(`Invalid opcode ${this.script[this.programCounter].toString(16)}`)
->>>>>>> fix pushdata
-=======
-    console.log('this.script', this.script)
-    if (!Script.opcodeIsValid(this.script.at(this.programCounter))) {
-      console.error('script invalid')
-      throw Error(`Invalid opcode ${this.script.at(this.programCounter).toString(16)}`)
->>>>>>> squashme
-    } else {
-      console.log('script valid')
-      if (this.script.at(this.programCounter) === 0x00) {
-        this.stack.push(Buffer.from('00', 'hex'))
-        this.programCounter += 1
-      } else if (this.script.at(this.programCounter) <= 0x4b) {
-        let bytesToPush = parseInt(this.script.at(this.programCounter), 16)
-
-        // Make sure there are this many bytes left to push
-        if (this.script.length - (this.programCounter + 1) < bytesToPush) {
-          throw Error(`Push bytes failed: script too small`)
-        }
-
-        // Push that many bytes to the stack
-        this.stack.push(this.script.slice(this.programCounter + 1, this.programCounter + 1 + bytesToPush))
-        this.programCounter += 1 + bytesToPush
-      } else {
-<<<<<<< HEAD
-        let opcode = bscript.opcodes.wordForOpcode(this.script[this.programCounter])
-        if (bscript.opcodes.wordIsDisabled(opcode)) {
->>>>>>> Add support for detecting invalid or disabled opcodes.
-=======
-        let opcode = Script.wordForOpcode(this.script.at(this.programCounter))
-        if (Script.wordIsDisabled(opcode)) {
->>>>>>> squashme
           throw Error(`Disabled opcode ${opcode}`)
         }
         // TODO: execute the op_code
         switch (opcode) {
           default:
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             throw Error(`Invalid opcode ${this.script.at(this.programCounter).toString(16)}`)
-=======
-            throw Error(`Invalid opcode ${this.script[this.pc].toString(16)}`)
->>>>>>> Add support for detecting invalid or disabled opcodes.
-=======
-            throw Error(`Invalid opcode ${this.script[this.programCounter].toString(16)}`)
->>>>>>> fix pushdata
-=======
-            throw Error(`Invalid opcode ${this.script.at(this.programCounter).toString(16)}`)
->>>>>>> squashme
         }
       }
     }
